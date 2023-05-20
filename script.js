6ec94832f8f87ea196a56d273fd37827f857cd31
Juli <julisoria07@gmail.com>
2023-05-19 22:31:10 -0300

    Iniciando de cero

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-function toggleNavbar() {
-    var navbarItems = document.getElementById("navbarItems");
-    navbarItems.classList.toggle("active");
-  }
-  
-  function toggleWeek(weekNumber) {
-    var weeks = document.getElementsByClassName("week-routines");
-    var buttons = document.getElementsByTagName("button");
-  
-    for (var i = 0; i < weeks.length; i++) {
-      weeks[i].classList.remove("active");
-      buttons[i].classList.remove("active");
-    }
-  
-    weeks[weekNumber - 1].classList.add("active");
-    buttons[weekNumber - 1].classList.add("active");
-  }
-  
+const botones = document.querySelectorAll('nav a');
+const pantallas = document.querySelectorAll('.pantalla');
+
+botones.forEach(boton => {
+  boton.addEventListener('click', () => {
+    const pantalla = boton.getAttribute('data-pantalla');
+    botones.forEach(boton => {
+      boton.classList.remove('activo');
+    });
+    boton.classList.add('activo');
+    pantallas.forEach(pantalla => {
+      pantalla.style.display = 'none';
+    });
+    document.querySelector(`.pantalla.${pantalla}`).style.display = 'block';
+  });
+});
